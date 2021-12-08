d359f777f67ac4cf048cafb7dc901875fc73d222
kazurayam <kazuaki.urayama@gmail.com>
2021-12-08 14:33:41 +0900

    organize imports

--- Scripts/TC1/Script1638843615597.groovy
+++ Scripts/TC1/Script1638843615597.groovy
@@ -1,34 +1,18 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-import org.openqa.selenium.chrome.ChromeDriver
-import com.kms.katalon.core.webui.driver.DriverFactory
-import org.openqa.selenium.remote.HttpCommandExecutor
-import org.openqa.selenium.remote.CommandExecutor
-
-import org.openqa.selenium.remote.SessionId
-import org.openqa.selenium.remote.RemoteWebDriver
-import org.openqa.selenium.remote.DesiredCapabilities
-import org.openqa.selenium.remote.Response
-import org.openqa.selenium.remote.Command
 import java.lang.reflect.Field
+
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.remote.Command
+import org.openqa.selenium.remote.CommandExecutor
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.HttpCommandExecutor
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.remote.Response
+import org.openqa.selenium.remote.SessionId
 import org.openqa.selenium.remote.http.W3CHttpCommandCodec
 import org.openqa.selenium.remote.http.W3CHttpResponseCodec
+
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 /**
  * https://tarunlalwani.com/post/reusing-existing-browser-session-selenium-java/
